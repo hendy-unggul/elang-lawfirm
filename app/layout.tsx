a5cff53c7214a2720dc46f3031b008e53c3f6f5b
NOPEfounderHD <nope.bukanguebanget@gmail.com>
2026-04-13 01:43:05 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,10 @@
-
+import './globals.css';
+import { Inter } from 'next/font/google';
+const inter = Inter({ subsets: ['latin'] });
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="id">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
