fc4f6cfafa575dc246725a02dd9405664aba66b9
NOPEfounderHD <nope.bukanguebanget@gmail.com>
2026-04-17 10:04:54 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,7 @@ export async function middleware(req: NextRequest) {
     {
       cookies: {
         getAll() { return req.cookies.getAll(); },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
           cookiesToSet.forEach(({ name, value, options }) => {
             req.cookies.set(name, value);
             res = NextResponse.next({ request: { headers: req.headers } });
